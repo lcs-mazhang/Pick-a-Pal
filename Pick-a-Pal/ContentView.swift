5d810d360da86ad120a85c349451fb8a44110f17
Matthew Zhang <gzhang@lcs.on.ca>
2023-12-07 17:37:32 -0500

    Adding ramdom name and picking

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -14,12 +14,33 @@ struct ContentView: View {
     
     @State private var names: [String] = ["Matthew", "AJ", "Yoshi", "Ong"]
     @State private var nameToAdd = ""
+    @State private var pickedName = ""
     
     var body: some View {
         VStack {
+            Text(pickedName.isEmpty ? " " : pickedName)
+            
             List {
                 ForEach(names, id: \.self) { name in
                     Text(name)
+                }
+            }
+            
+            // Using name to add var to let us adding more contact list
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Button("Pick Random Name") {
+                if names.count > 0 {
+                    pickedName = names.randomElement()!
+                } else {
+                    pickedName = ""
                 }
             }
         }
